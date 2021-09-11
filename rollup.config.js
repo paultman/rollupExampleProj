a8a9c080e5e5327351d69d8fc981bf3f645fea86
paultman <paultman@gmail.com>
2021-09-11 07:33:35 -0500

    add minify plugin and call from output config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import copy from 'rollup-plugin-copy';
 import serve from 'rollup-plugin-serve';
+import {terser} from 'rollup-plugin-terser';
 
 export default {
   input: 'src/js/index.js',
-  output: { file: 'dist/js/index.min.js', format: 'iife', sourcemap: 'inline' },
+  output: { file: 'dist/js/index.min.js', format: 'iife', sourcemap: 'inline', plugins: [terser()] },
   plugins: [
     copy({
       targets: [{ src: 'src/index.html', dest: 'dist/' }],
